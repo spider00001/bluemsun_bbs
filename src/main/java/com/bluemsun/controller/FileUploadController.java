03aaca6e1138f1eb75f7dce208454e8ee378a7e0
spider00001 <81937944+spider00001@users.noreply.github.com>
2021-10-21 16:41:29 +0800

    实现了头像上传

--- src/main/java/com/bluemsun/controller/FileUploadController.java
+++ src/main/java/com/bluemsun/controller/FileUploadController.java
@@ -1,44 +1,81 @@
 package com.bluemsun.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
-@Controller
-public class FileUploadController extends HttpServlet {
-    @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
-    @ResponseBody
-    public  String uploadFileHandler( @RequestParam("file") MultipartFile file) {
+@RequestMapping("/user")
+@RestController
+public class FileUploadController {
 
-        if (!file.isEmpty()) {
-            try {
-                HttpServletRequest req;
-
-                // 文件存放服务端的位置
-                String rootPath = "d:/tmp";
-                File dir = new File(rootPath + File.separator + "tmpFiles");
-                if (!dir.exists())
-                    dir.mkdirs();
-                // 写文件到服务器
-                File serverFile = new File(dir.getAbsolutePath() + File.separator + file.getOriginalFilename());
-                file.transferTo(serverFile);
-                return "You successfully uploaded file=" +  file.getOriginalFilename();
-            } catch (Exception e) {
-                return "You failed to upload " +  file.getOriginalFilename() + " => " + e.getMessage();
+    //用户头像上传
+    @PostMapping(value="/uploadHeadPortrait")
+    public Map uploadHeadPortrait(@RequestParam("file") MultipartFile file, HttpServletRequest request) {
+        Map<String,Object> map = new HashMap<String,Object>();
+        try {
+            String disposition = file.getOriginalFilename();
+            String suffix = disposition.substring(disposition.lastIndexOf("."));
+            if (suffix.contains("png")||suffix.contains("jpg")) {
+                //随机生成的UUID，作为文件名的一部分，加上刚才获取到的后缀作为最终文件名
+                String filename = UUID.randomUUID()+suffix;
+                String serverPath = request.getSession().getServletContext().getRealPath("headPortrait");
+                File fileDisk = new File(serverPath);
+                if (!fileDisk.exists()) {
+                    fileDisk.mkdir();
+                }
+                String fileParts = serverPath + "/" + filename;
+                file.transferTo(new File(fileParts));
+                String projectServerPath = request.getScheme() + "://" + request.getServerName() +
+                        ":" + request.getServerPort() + request.getContextPath() + "/headPortrait/" + filename;
+                map.put("msg","上传成功");
+                map.put("status",1);
+                map.put("headPortrait",projectServerPath);
+            } else {
+                map.put("msg","上传失败，只支持.png或.jpg格式的图片");
+                map.put("status",2);
             }
-        } else {
-            return "You failed to upload " +  file.getOriginalFilename() + " because the file was empty.";
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return map;
+//        if(!file.isEmpty()) {
+//            try {
+//                String disposition = file.getOriginalFilename();
+//                String suffix = disposition.substring(disposition.lastIndexOf("."));
+//                if (suffix.contains("png")||suffix.contains("jpg")) {
+//
+//                }
+//                //随机生成的UUID，作为文件名的一部分，加上刚才获取到的后缀作为最终文件名
+//                String filename = UUID.randomUUID()+suffix;
+//                String serverPath = request.getSession().getServletContext().getRealPath("headPortrait");
+//                File fileDisk = new File(serverPath);
+//                if (!fileDisk.exists()) {
+//                    fileDisk.mkdir();
+//                }
+//                String fileParts = serverPath + "/" + filename;
+//                file.transferTo(new File(fileParts));
+//                projectServerPath = request.getScheme() + "://" + request.getServerName() +
+//                        ":" + request.getServerPort() + request.getContextPath() + "/headPortrait/" + filename;
+//
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//            map.put("msg","上传成功");
+//            map.put("status",1);
+//            map.put("headPortrait",projectServerPath);
+//            return map;
+//        } else {
+//            map.put("msg","上传失败，图片格式不支持");
+//            map.put("status",2);
+//            return map;
+//        }
     }
+
 
 }
